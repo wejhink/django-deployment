f9da916efc6998a6e37c9e93b17942942a14d4cd
Ray Ch <i@iraycd.com>
2016-08-04 15:46:05 +0530

    Initial Commit

---
+++ fabfile/__init__.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import with_statement
+
+from contextlib import contextmanager as _contextmanager
+
+from fabric.api import run, env, run, cd, prefix, local
+# extra content
+from django_commands import *
+from nginx_commands import *
+from supervisor_commands import *
+from ubuntu_commands import *
+
+
+
+env.hosts = ['{{cookiecutter.server_ip}}']
+env.user = 'root'
+env.directory = '/www/{{cookiecutter.app_name}}/'
+env.activate = 'source /www/{{cookiecutter.app_name}}-package/bin/activate'
+
+
+@_contextmanager
+def virtualenv():
+    "Activate virtualenv"
+    with cd(env.directory):
+        with prefix(env.activate):
+            yield
+
+
+def py(context):
+    "Execute python command"
+    run("python " + context)
+
+
+def info():
+    "Show host name"
+    run('uname -a')
+
+
+def update():
+    "Update server content"
+    local("git push")
+    with cd(env.directory):
+        run('git fetch --all')
+        run('git reset --hard origin/master')
+
+
+def pip(*args):
+    "Install package on server. Usage: pip:install,easy_thumbnails"
+    with virtualenv():
+        run("pip " + " ".join(args))
+
+
+def load_requirements():
+    "Install requirements packages"
+    with cd(env.directory):
+        with virtualenv():
+            run("pip install -r requirements.txt")
+
+
+def service(*args):
+    "Control Services. Usage:start,nginx"
+    run("service " + " ".join(args))
+
+
+def omo():
+    "Lava mais branco"
+    with cd(env.directory):
+        run('find -name "*.pyc" -delete')
+
+
+def deploy():
+    "Update git repository and reset nginx"
+    update()
+    omo()
+    syncdb()
+    static()
+    supervisor_reset_gunicorn()
---
+++ fabfile/ubuntu_commands.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from fabric.api import run, cd
+
+APT_GET_PACKAGES = [
+    "build-essential",
+    "git",
+    "vim",
+    "python-dev",
+    "python-virtualenv",
+    "python-pip",
+    "libpq-dev",
+    "postgresql",
+    "postgresql-contrib",
+    "nginx",
+    "libxml2-dev",
+    "libxslt1-dev",
+    "lib32z1-dev",
+    "libffi-dev", #Requirement of cffi
+    "libssl-dev",
+    "redis-server", #Using Redis as Django's session store and cache backend.
+    "libtiff5-dev libjpeg8-dev libopenjpeg-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python-tk",
+    "elasticsearch"
+]
+
+
+def setup():
+    "Install default packages for django"
+    run("apt-get install " + " ".join(APT_GET_PACKAGES))
+
+
+def setup_webserver():
+    "Install default packages for django and NGINX"
+    APT_GET_PACKAGES.append("nginx")
+    setup()
+
+
+def create_www():
+    "Configure permissions on www"
+    run("mkdir -p /www/")
+    run("chown -R root:www-data /www/")
+    run("chmod 775 -R /www/")
+    run("chmod g+s -R /www/")
+
+def create_clone():
+    with cd("/www/"):
+        run("git clone git@bitbucket.org:{{cookiecutter.bitbucket_repository_user}}/{{cookiecutter.app_name}}.git")
+
+def create_package(name):
+    "Create virtualenv"
+    create_www()
+    package_name = "/www/%s-package" % str(name)
+    run("virtualenv " + package_name)
+    with cd(package_name):
+        run("mkdir -p logs")
